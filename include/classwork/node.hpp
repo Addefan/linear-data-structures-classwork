80476de4d2e2b2604053915af7dbe0bd50e4a7c6
Ramil Safin <RamilSafNab1996@gmail.com>
2022-02-27 22:03:07 +0300

    Add double-linked node

--- include/classwork/node.hpp
+++ include/classwork/node.hpp
@@ -20,4 +20,20 @@ namespace classwork {
     explicit Node(int value, Node* next = nullptr);
   };
 
+  struct DoubleNode {
+    // поля структуры
+    int value{0};
+    Node* prev{nullptr};  // указатель на предыдущий узел
+    Node* next{nullptr};  // указатель на следующий узел
+
+    /**
+     * Создание узла со значением и указателем на предыдущий и следующий узлы.
+     *
+     * @param value - значение узла\
+     * @param prev - указатель на предыдущий узел
+     * @param next - указатель на следующий узел
+     */
+    explicit DoubleNode(int value, Node* prev = nullptr, Node* next = nullptr);
+  };
+
 }  // namespace classwork
--- src/node.cpp
+++ src/node.cpp
@@ -4,4 +4,5 @@ namespace classwork {
 
   Node::Node(int value, Node* next) : value{value}, next{next} {}
 
+  DoubleNode::DoubleNode(int value, Node *prev, Node *next) : value{value}, prev{prev}, next{next} {}
 }  // namespace classwork
