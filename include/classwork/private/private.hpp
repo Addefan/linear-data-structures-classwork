fa0ee578d77ba6b23e24fd1378afb0d1fd7a53b5
Ramil Safin <RamilSafNab1996@gmail.com>
2022-02-28 11:23:34 +0300

    Refactor check_out_of_range function

--- include/classwork/private/private.hpp
+++ include/classwork/private/private.hpp
@@ -5,14 +5,21 @@
 
 namespace classwork::internal {
 
+  /**
+   * Проверка вхождения индекса в полуинтервал [min, max).
+   *
+   * @param index - значение индекса
+   * @param min - нижняя граница (включительно)
+   * @param max - верхняя граница (не включительно)
+   * @throws out_of_range при выходе индекса за границы полуинтервала
+   */
   inline void check_out_of_range(int index, int min, int max) {
-    if (index >= min && index < max) {
-      return;
-    }
+    if (index < min || index >= max) {
+      std::stringstream ss("index is out of range: ");
+      ss << '[' << min << ',' << max << ')';
 
-    std::stringstream ss("index is out of range: ");
-    ss << '[' << min << ',' << max << ']';
-    throw std::out_of_range(ss.str());
+      throw std::out_of_range(ss.str());
+    }
   }
 
 }  // namespace classwork::internal
